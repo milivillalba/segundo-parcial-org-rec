190fa5928b8b097a0722cdbc2912eb93ffb4324b
milivillalba <soledad110203@gmail.com>
2023-07-05 15:10:50 -0300

    configuracion del app.js implementacion de middlewares

--- app.js
+++ app.js
@@ -1,20 +1,41 @@
 // Imports
-const cors = require('cors');
-const express = require('express');
 
-const path = require('path');
+require("dotenv").config();
+const cors = require("cors");
+const express = require("express");
+const helmet = require("helmet");
+const morgan = require("morgan");
+const path = require("path");
+const port = process.env.PORT || 5000;
 
 const app = express();
 
+require("ejs");
+
+// Se importa la instancia de conexión a la base de datos - (debe ser después de leer las variables de entorno)
+const { sequelize } = require("./database");
+
+// Conexión a base de datos
+sequelize
+  .authenticate()
+  .then(() => console.log("Conexión a base de datos exitosa"))
+  .catch((error) => console.log("Error al conectar a base de datos", error));
+
 // Middlewares
 // TODO: Implementar middlewares
+app.use(cors());
+// app.use(helmet());
+app.use(morgan("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.set("view engine", "ejs");
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
 // Routes
-app.use('/api', require('./routes/reserva.routes'));
+app.use("/api", require("./routes/reserva.routes"));
 
 // TODO: Si la petición no coincide con ninguna de las rutas declaradas, mostrar error 404
 
 // Starting the server
-app.listen(45635, () => console.log('Server on port xxxx'));
+app.listen(port, () => console.log(`Server on http://localhost:${port}`));
